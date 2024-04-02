9aef933d33ca5219e0da3165f5ef29655d59c75c
pgg1309 <pgrahl@gmail.com>
2024-04-02 00:32:24 -0300

    fixed

--- R/fused_ridge-constructor.R
+++ R/fused_ridge-constructor.R
@@ -1,4 +1,4 @@
-new_fused_ridge <- function(coef_values, lambda, positive, coef_names, blueprint) {
+new_fused_ridge <- function(coef_values, lambda, positive, deriv2_smooth, coef_names, blueprint) {
   hardhat::new_model(
     coef_values = coef_values,
     coef_names = coef_names,
